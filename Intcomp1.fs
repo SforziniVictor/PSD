0d017f042655f1e63a22e05fd2df2b538f1c5a61
Lucas <lucasveha@gmail.com>
2025-09-04 14:54:41 +0200

    message

--- Intcomp1.fs
+++ Intcomp1.fs
@@ -29,8 +29,83 @@ let rec eval e (env : (string * int) list) : int =
           let xval = eval e env
           let env1 = (s, xval) :: env 
           letEval x env1 
+        | (s, e) :: [] ->
+          let xval = eval e env
+          let env1 = (s, xval) :: env 
+          env1
+        | _ -> env
       eval ebody (letEval varExpress env)
     | Prim("+", e1, e2) -> eval e1 env + eval e2 env
     | Prim("*", e1, e2) -> eval e1 env * eval e2 env
     | Prim("-", e1, e2) -> eval e1 env - eval e2 env
     | Prim _            -> failwith "unknown primitive";;
+
+let e1 = eval (Let([("x1", CstI 1); ("x2", CstI 5)], Prim("+", Var "x1", Var "x2"))) [];;
+let e2 = eval (Let([("x1", CstI 2); ("x2", Prim("+", Var "x1", CstI 7))], Prim("+", Var "x1", Var "x2"))) [];;
+
+let run e = eval e [];;
+
+(* ---------------------------------------------------------------------- *)
+
+(* Closedness *)
+
+// let mem x vs = List.exists (fun y -> x=y) vs;;
+
+let rec mem x vs = 
+    match vs with
+    | []      -> false
+    | v :: vr -> x=v || mem x vr;;
+
+(* Checking whether an expression is closed.  The vs is 
+   a list of the bound variables.  *)
+
+(* ---------------------------------------------------------------------- *)
+
+(* Substitution of expressions for variables *)
+
+(* This version of lookup returns a Var(x) expression if there is no
+   pair (x,e) in the list env --- instead of failing with exception: *)
+
+let rec lookOrSelf env x =
+    match env with 
+    | []        -> Var x
+    | (y, e)::r -> if x=y then e else lookOrSelf r x;;
+
+(* Remove (x, _) from env: *)
+
+let rec remove env x =
+    match env with 
+    | []        -> []
+    | (y, e)::r -> if x=y then r else (y, e) :: remove r x;;
+
+(* Naive substitution, may capture free variables: *)
+let rec union (xs, ys) = 
+    match xs with 
+    | []    -> ys
+    | x::xr -> if mem x ys then union(xr, ys)
+               else x :: union(xr, ys);;
+
+(* minus xs ys  is the set of all elements in xs but not in ys *)
+
+let rec minus (xs, ys) = 
+    match xs with 
+    | []    -> []
+    | x::xr -> if mem x ys then minus(xr, ys)
+               else x :: minus (xr, ys);;
+
+(* Find all variables that occur free in expression e *)
+
+let rec freevars e : string list =
+    match e with
+    | CstI i -> []
+    | Var x  -> [x]
+    | Let(xList, ebody) -> 
+          let rec freemanyvars (xList : (string * expr) list) : string list =
+            match xList with
+            | [] -> []
+            | (_, e) :: x -> union (freevars e, freemanyvars x)
+          let bounded = List.map fst xList
+          let defs = freemanyvars xList
+          let body = minus (freevars ebody, bounded)
+          union (defs, body)
+    | Prim(ope, e1, e2) -> union (freevars e1, freevars e2);;
